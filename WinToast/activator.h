2378b7d5103f8c7f20934b26b7f94ff6234f3333
malja <looorin@gmail.com>
2018-11-04 14:52:44 +0100

    Simplified Activator class

--- WinToast/activator.h
+++ WinToast/activator.h
@@ -17,17 +17,7 @@ class DECLSPEC_UUID("8aa59b6c-1401-42a5-b1cf-3ccb3b360e77") Activator WrlSealed 
 
 	HRESULT STDMETHODCALLTYPE Activate(_In_ LPCWSTR appUserModelId, _In_ LPCWSTR invokedArgs, _In_reads_(dataCount) const NOTIFICATION_USER_INPUT_DATA* data, ULONG dataCount) override {
 
-        // Change username
-		std::wofstream file("C:\\Users\\janma\\Desktop\file.txt");
-
-		file << appUserModelId << std::endl;
-		file << invokedArgs << std::endl;
-
-		for (ULONG i = 0; i < dataCount; i++) {
-			file << (data[i]).Key << " " << (data[i]).Value << std::endl;
-		}
-
-        file.close();
+        MessageBox( 0, L"Hello World!", L"Activator", 0 );
 		return S_OK;
 	}
 };
