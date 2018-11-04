6aab18cffe36509357ed9d21e72c646c3da42b7f
malja <looorin@gmail.com>
2018-11-04 14:53:21 +0100

    Simplified main class

--- WinToast/main.cpp
+++ WinToast/main.cpp
@@ -3,7 +3,7 @@
 #include "wintoastlib.h"
 #include "event_handler.h"
 #include <string>
-#include <activator_helper.h>
+#include "activator_helper.h"
 
 #include "activator.h"
 
@@ -12,26 +12,19 @@ using namespace DesktopNotificationManagerCompat;
 
 int main(int argc, char *argv[]) {
 
+    CoInitialize(nullptr);
+
     // Check if application was executed with parameter -ToastActivated
     for (int i = 0; i < argc; i++) {
 
-        std::cout << "Param: " << i << argv[i] << std::endl;
-
         if (strcmp(argv[i], "-ToastActivated") == 0) {
-
-            std::ofstream f;
-            f.open("C:\\Users\\janma\\Desktop\\test.txt");
-            f << "Toast activated" << std::endl;
-            f.close();
-
-            std::cout << "Activated from notification" << std::endl;
-            std::cout << "Param: " << argv[1] << std::endl;
+            MessageBox(0, L"Toast was activated!", L"WinToast", 0 );
             return 0;
         }
     }
 
     // Parameters for notification
-    std::wstring appName(L"Console WinToast Example");
+    std::wstring appName(L"WinToast"); // Acts as .lnk file name in Startup directory.
     std::wstring aumi(L"company.product");
     std::wstring text(L"Text");
 
@@ -42,16 +35,18 @@ int main(int argc, char *argv[]) {
 
     if (!SUCCEEDED(hr)) {
         std::cerr << "Error: Unable to register AUMID" << std::endl;
+        return 1;
     }
 
     hr = RegisterActivator();
     if (!SUCCEEDED(hr)) {
-        std::cerr << "error: Unable to register activator" << std::endl;
+        std::cerr << "error: Unable to register activator: '" << std::hex << hr << "'" << std::endl;
+        return 2;
     }
 
 	if (!WinToast::isCompatible()) {
 		std::cerr << "Error, your system in not supported!" << std::endl;
-        return -1;
+        return 3;
 	}
 
 	WinToast::instance()->setAppName(appName);
@@ -59,18 +54,17 @@ int main(int argc, char *argv[]) {
 
 	if (!WinToast::instance()->initialize()) {
 		std::cerr << "Error, your system in not compatible!" << std::endl;
-		return -2;
+		return 4;
 	}
 
     // Use ::Generic for new TemplateType which should support Activator
     // Or any of other types.
 	WinToastTemplate templ(WinToastTemplate::Generic);
-
 	templ.setTextField(text, WinToastTemplate::FirstLine);
 
     if (WinToast::instance()->showToast(templ, new EventHandler()) < 0) {
 	    std::cerr << "Could not launch your toast notification!";
-		return -3;
+		return 5;
 	}
 
 	Sleep(15000);
